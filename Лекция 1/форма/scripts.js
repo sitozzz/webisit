dbb7c4ed14bb205675e42cecea9d50a71d57f9c9
sitozzz <sit7602@gmail.com>
2018-02-19 23:38:00 +0500

    Рефакторинг + оформление формы

--- Лекция 1/форма/scripts.js
+++ Лекция 1/форма/scripts.js
@@ -1,22 +1,21 @@
 //Проверка размера загружаемого файла
 function updateSize() {
-    var file = document.getElementById("Photo").files[0],
-      ext = "не определилось",
-      parts = file.name.split('.');
-    if (parts.length > 1) ext = parts.pop();
+  var file = document.getElementById("Photo").files[0],
+    ext = "не определилось",
+    parts = file.name.split('.');
+  if (parts.length > 1) ext = parts.pop();
+  document.getElementById("fileinfo").innerHTML = [
+    "Размер файла: " + file.size + " B",
+    "Расширение: " + ext,
+    "MIME тип: " + file.type
+  ].join("<br>");
+  if (file.size > 1000000) {
+    alert('Невозможно загрузить файл больше 1Мб!');
+    var inp = document.querySelector("#Photo");
+    inp.value = "";
     document.getElementById("fileinfo").innerHTML = [
+      "Превышен допустимый размер файла!",
       "Размер файла: " + file.size + " B",
-      "Расширение: " + ext,
-      "MIME тип: " + file.type
     ].join("<br>");
-    if (file.size > 1000000) {
-      alert('Невозможно загрузить файл больше 1Мб!');
-      var inp=document.querySelector("#Photo");
-      inp.value = "";
-      document.getElementById("fileinfo").innerHTML = [
-        "Превышен допустимый размер файла!",
-        "Размер файла: " + file.size + " B",
-      ].join("<br>");
-    }
   }
-  
+}
